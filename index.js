cee70d3023b7805a9e5c717c931a47e973669eeb
AnjaleeNeelika <108944238+AnjaleeNeelika@users.noreply.github.com>
2025-06-14 14:21:12 +0530

    Connect MongoDB database

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+// Load environment variables
+require('dotenv').config()
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from Node API')
+});
+
+// DB Connection
+mongoose.connect(process.env.DB_URL)
+.then(() => {
+    console.log("Connected to the database");
+    app.listen(process.env.PORT || 3000, () => {
+        console.log("Server is running on port " + process.env.PORT || 3000);
+    });
+})
+.catch(() => {
+    console.log("Connection failed");
+})
